a1b75d3d6dfd3ff9bc2a0797c2b903655d34a4fc
AbroadGames <AbroadGames@users.noreply.github.com>
2020-11-26 18:05:36 +0000

    Create Inital Controllers
    Created contoller templates for all exsisting models

---
+++ Authentication API/Authentication API/Controllers/PasswordController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Authentication_API.Models;
+
+namespace Authentication_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordController : ControllerBase
+    {
+        private readonly COMP2001_DThomasContext _context;
+
+        public PasswordController(COMP2001_DThomasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Password
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Password>>> GetPasswords()
+        {
+            return await _context.Passwords.ToListAsync();
+        }
+
+        // GET: api/Password/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Password>> GetPassword(int id)
+        {
+            var password = await _context.Passwords.FindAsync(id);
+
+            if (password == null)
+            {
+                return NotFound();
+            }
+
+            return password;
+        }
+
+        // PUT: api/Password/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPassword(int id, Password password)
+        {
+            if (id != password.PasswordId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(password).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PasswordExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Password
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Password>> PostPassword(Password password)
+        {
+            _context.Passwords.Add(password);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPassword", new { id = password.PasswordId }, password);
+        }
+
+        // DELETE: api/Password/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Password>> DeletePassword(int id)
+        {
+            var password = await _context.Passwords.FindAsync(id);
+            if (password == null)
+            {
+                return NotFound();
+            }
+
+            _context.Passwords.Remove(password);
+            await _context.SaveChangesAsync();
+
+            return password;
+        }
+
+        private bool PasswordExists(int id)
+        {
+            return _context.Passwords.Any(e => e.PasswordId == id);
+        }
+    }
+}
---
+++ Authentication API/Authentication API/Controllers/SessionController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Authentication_API.Models;
+
+namespace Authentication_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        private readonly COMP2001_DThomasContext _context;
+
+        public SessionController(COMP2001_DThomasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Session
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Session>>> GetSessions()
+        {
+            return await _context.Sessions.ToListAsync();
+        }
+
+        // GET: api/Session/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Session>> GetSession(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return session;
+        }
+
+        // PUT: api/Session/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSession(int id, Session session)
+        {
+            if (id != session.SessionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(session).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SessionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Session
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Session>> PostSession(Session session)
+        {
+            _context.Sessions.Add(session);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSession", new { id = session.SessionId }, session);
+        }
+
+        // DELETE: api/Session/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Session>> DeleteSession(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sessions.Remove(session);
+            await _context.SaveChangesAsync();
+
+            return session;
+        }
+
+        private bool SessionExists(int id)
+        {
+            return _context.Sessions.Any(e => e.SessionId == id);
+        }
+    }
+}
