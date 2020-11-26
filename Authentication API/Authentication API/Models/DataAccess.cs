5e6e47d2e6a4c3bb286b4b20973fa7c0977e439f
AbroadGames <AbroadGames@users.noreply.github.com>
2020-11-26 18:22:41 +0000

    Create DataAccess.cs
    Created inital DataAccess class.

---
+++ Authentication API/Authentication API/Models/DataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication_API.Models
+{
+    public class DataAccess
+    {
+        private String Connection { get; set; }
+
+        public bool Validate(User user)
+        {
+            if (user.Email == "")
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+
+        public void Register(User user)
+        {
+
+        }
+
+        public void Update(User user, int number)
+        {
+
+        }
+
+        public void Delete(int number)
+        {
+
+        }
+
+    }
+}
