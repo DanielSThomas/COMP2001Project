541a07ff22c2e18a5d8c1ac0df384e07fbed86e8
DanielSThomas <dansebtom@yahoo.co.uk>
2021-01-13 16:16:17 +0000

    Update usp_register.sql
    Temporary testing of output message.

--- docs/QueryLibrary/usp_register.sql
+++ docs/QueryLibrary/usp_register.sql
@@ -7,8 +7,8 @@ CREATE PROCEDURE dbo.usp_register
 @newFirstName VARCHAR(50),
 @newLastName VARCHAR(50),
 @newEmail VARCHAR(50),
-@newPassword VARCHAR(50)
-
+@newPassword VARCHAR(50),
+@message VARCHAR(200) OUTPUT
 AS
 BEGIN TRANSACTION
 declare @errorMessage INT
@@ -21,13 +21,16 @@ SET XACT_ABORT ON /*Automaticly Rollback Transaction if a runtime error happens*
 IF EXISTS (SELECT 1 FROM dbo.Users WHERE dbo.Users.FirstName = @newFirstName AND dbo.Users.LastName = @newLastName)
     BEGIN
         PRINT 'ERROR: User Name Already Exsits In Database!'
-        RETURN
+        SET @message = 'Register Failed : User Name Already Exsits In Database!'
+        RETURN 
     END
 ELSE
     BEGIN TRY
         INSERT INTO dbo.Users (FirstName, LastName, Email, CurrentPassword)
         VALUES (@newFirstName,@newLastName, @newEmail, @newPassword)
         COMMIT TRANSACTION
+        SET @message = 'Register Success'
+        
     END TRY
 
     BEGIN CATCH      
@@ -35,13 +38,14 @@ ELSE
         SET @errorMessage = ERROR_MESSAGE()
         SET @errorServerity = ERROR_SEVERITY()
         SET @errorState = ERROR_STATE()
-        RAISERROR(@errorMessage,@errorServerity,@errorState)           
+        RAISERROR(@errorMessage,@errorServerity,@errorState)
+        SET @message = 'Register Failed : General Failure'           
     END CATCH
 
 GO
 
 
-EXEC dbo.usp_register 'Larry', 'Fisher','AGoodEmail@Email.com','LookaPassW0rd'
+EXEC dbo.usp_register 'Will2', 'Fisher','ABadEmail3@Email.com','LookaBadW0rdAgain',0
 GO
 
 SELECT * FROM dbo.Users;
