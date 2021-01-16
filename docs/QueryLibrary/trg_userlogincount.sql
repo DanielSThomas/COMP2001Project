4fd02b0a2aed07bc534fb3c6cc87413379b9af11
DanielSThomas <dansebtom@yahoo.co.uk>
2021-01-16 22:00:12 +0000

    Create trg_userlogincount.sql
    Trigger for updateing the user session count.

---
+++ docs/QueryLibrary/trg_userlogincount.sql
@@ -0,0 +1,21 @@
+/*Customer Visit Count Trigger*/
+IF OBJECT_ID('dbo.trg_UserLoginCount') IS NOT NULL
+    DROP PROCEDURE dbo.trg_UserLoginCount
+GO
+
+Create TRIGGER dbo.trg_UserLoginCount
+ON dbo.Sessions
+AFTER INSERT
+AS
+BEGIN
+declare @UserID INT
+
+SET NOCOUNT ON;
+
+SELECT @UserID = UserID from inserted
+UPDATE dbo.LoginCounter
+SET LoginCount = LoginCount +1
+WHERE UserID = @UserID
+
+END
+
