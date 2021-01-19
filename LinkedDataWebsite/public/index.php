21f4e321dc561ea42914d0ce35d66d4d01cd86f8
DanielSThomas <dansebtom@yahoo.co.uk>
2021-01-19 11:55:45 +0000

    Initial structure of linked data website

---
+++ LinkedDataWebsite/public/index.php
@@ -0,0 +1,22 @@
+
+
+    <body class="bg-info"></body>
+    <div class="container-fluid col-md-10offset-md-1">
+        <div class=’row’>
+            <div class="card mt-3 px-2 py-2">
+                <h1>Test String</h1>
+                <p>"Test Paragraph”</p>
+                <form action="testPage.php" method="GET">
+                <div class="form-check">
+                    <label for=agree">I agree</label><input type="radio" id=agree" name="Test" value="true">
+
+                    <label for=disagree">I disagree</label><input type="radio" id=disagree" name="Test" value="false"
+                </div>
+                    <div>
+                        <input type="submit" value="Submit" id="Submit" class="btn btn-primary nb-2"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
