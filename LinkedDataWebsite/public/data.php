7dd4bbfc8718f523bcef6d755e94c495d0c9dc54
DanielSThomas <dansebtom@yahoo.co.uk>
2021-01-20 15:25:41 +0000

    Inital bootstrap and page layouts as well as local data

---
+++ LinkedDataWebsite/public/data.php
@@ -0,0 +1,28 @@
+
+<head>
+    <title>Bootstrap Test</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">Linked Data Site</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home Page</a></li>
+            <li><a href="data.php">Data</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="jumbotron">
+    <div class="container text-center">
+        <h1>Linked Data</h1>
+        <p>Here is where the data should be</p>
+    </div>
+</div>
--- LinkedDataWebsite/public/index.php
+++ LinkedDataWebsite/public/index.php
@@ -1,22 +1,54 @@
 
+<head>
+    <title>Bootstrap Test</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
 
-    <body class="bg-info"></body>
-    <div class="container-fluid col-md-10offset-md-1">
-        <div class=’row’>
-            <div class="card mt-3 px-2 py-2">
-                <h1>Test String</h1>
-                <p>"Test Paragraph”</p>
-                <form action="testPage.php" method="GET">
-                <div class="form-check">
-                    <label for=agree">I agree</label><input type="radio" id=agree" name="Test" value="true">
 
-                    <label for=disagree">I disagree</label><input type="radio" id=disagree" name="Test" value="false"
-                </div>
-                    <div>
-                        <input type="submit" value="Submit" id="Submit" class="btn btn-primary nb-2"/>
-                    </div>
-                </form>
-            </div>
+<body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Linked Data Site</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home Page</a></li>
+            <li><a href="data.php">Data</a></li>
+        </ul>
+    </div>
+</nav>
+
+
+    <div class="jumbotron">
+        <div class="container text-center">
+        <h1>Linked Data Site</h1>
+        <p>A linked data website with data or something</p>
         </div>
     </div>
+<div class="container text-center">
+    <div class="row">
+        <div class="col-sm-6">
+            <h3>Column 1</h3>
+            <p>Something about the data</p>
+            <p>More text</p>
+        </div>
+        <div class="col-sm-6">
+            <h3>Column 2</h3>
+            <p>Look more text</p>
+            <p>Less text</p>
+        </div>
+    </div>
+</div>
+
+<div class="container text-center">
+    <h3>Original Data: </h3>
+    <p>link to data</p>
+</div>
+
+</body>
 
